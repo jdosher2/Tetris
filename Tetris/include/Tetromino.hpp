eef1b13452e2c8966a8a9d567d87151588a6b67a
julie <jdosher2@illinois.edu>
2018-11-15 13:03:49 -0600

    got rid of some unneeded lines

--- Tetris/include/Tetromino.hpp
+++ Tetris/include/Tetromino.hpp
@@ -38,25 +38,6 @@ private:
     static void InitShapes();
 };
 
-
-//extern const std::map<char, std::array<int, Tetromino::kTetrominoSize>> all_shapes = {
-    
-    /*
-     using pattern:
-     0   1
-     2   3
-     4   5
-     6   7
-    {'I', {0, 2, 3, 6} },
-    {'J', {3, 5, 7, 6} },
-    {'L', {2, 4, 6, 7} },
-    {'O', {2, 3, 4, 5} },
-    {'S', {3, 5, 4, 6} },
-    {'T', {3, 5, 4, 7} },
-    {'Z', {2, 4, 5, 7} }
-     */
-
-// red, orange, green, blue, cyan, magenta, yellow
 extern const std::map<char, ofColor> shapes_and_colors = {
     {'I', ofColor::cyan},
     {'J', ofColor::orange},
